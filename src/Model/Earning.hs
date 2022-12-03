7395f6ada4856d828631156de8bc8ffb3b8e0526
hamburgersct <hamburgersct@gmail.com>
2022-12-03 11:27:08 -0800

    Completed Earning model

---
+++ src/Model/Earning.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE RecordWildCards #-}
+module Model.Earning
+    (
+        -- * Types
+        Earning,
+        -- * Earning APIs
+        init,
+        add,
+        detract,
+        resetEarning,
+        setMaxEarning,
+        getCurrEarning,
+        getMaxEarning
+    )
+    where
+
+import Prelude hiding (init)
+
+------------------------------
+-- Earning model
+------------------------------
+
+data Earning = Earning
+    {
+        eKitchen    :: Int  -- current earning
+        eMax        :: Int  -- max earning on record
+    }
+    deriving (Eq, Ord, Show)
+
+init :: Int -> Earning
+init num = Earning num num
+
+add :: Earning -> Earning
+add e = e {eKitchen = eKitchen e + 1}
+
+detract :: Earning -> Earning
+detract e = e {eKitchen = eKitchen e - 1}
+
+resetEarning :: Earning -> Earning
+resetEarning e = e {eKitchen = 0}
+
+setMaxEarning :: Earning -> Earning
+setMaxEarning e = e {eMax = if (curr > max) then curr else max}
+    where curr = getCurrEarning e 
+            max = getMaxEarning e 
+
+getCurrEarning :: Earning -> Int
+getCurrEarning Earning {..} = eKitchen
+
+getMaxEarning :: Earning -> Int
+getMaxEarning Earning {..} = eMax
