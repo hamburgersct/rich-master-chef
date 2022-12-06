6176fb08e656389c1efded15c8e932d5fb1c7277
hamburgersct <hamburgersct@gmail.com>
2022-12-06 15:51:25 -0800

    Added comments

--- src/Control.hs
+++ src/Control.hs
@@ -2,88 +2,97 @@ module Control where
 
 import Brick hiding (Result)
 import qualified Brick.Types as T
--- import System.Random hiding(next)
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Graphics.Vty as V
-
-
 import Model
-import Model.Kitchen
 import Model.Budget
 import Model.Date
 import Model.Earning
+import Model.Kitchen
 import Model.Player
-import Control.Monad.IO.Class (MonadIO(liftIO))
 
 control :: PlayerState -> BrickEvent n Tick -> EventM n (Next PlayerState)
 control ps event = case event of
-    T.VtyEvent (V.EvKey V.KUp   _)  -> Brick.continue (move up    ps)
-    T.VtyEvent (V.EvKey V.KDown _)  -> Brick.continue (move down  ps)
-    T.VtyEvent (V.EvKey V.KLeft _)  -> Brick.continue (move left  ps)
-    T.VtyEvent (V.EvKey V.KRight _) -> Brick.continue (move right ps)
-    T.VtyEvent (V.EvKey (V.KChar 'a') _) -> nextPlace ps   =<< liftIO (kitchenPlaceA ps)
-    T.VtyEvent (V.EvKey (V.KChar 'b') _) -> nextPlace ps   =<< liftIO (kitchenPlaceB ps)
-    T.VtyEvent (V.EvKey (V.KChar 'p') _) -> nextPrepare ps   =<< liftIO (kitchenPrepare ps)
-    T.VtyEvent (V.EvKey (V.KChar 'k') _) -> nextCook ps   =<< liftIO (kitchenCook ps)
-    T.VtyEvent (V.EvKey (V.KChar 's') _) -> nextSeason ps   =<< liftIO (kitchenSeason ps)
-    T.VtyEvent (V.EvKey (V.KChar 'c') _) -> nextClean ps   =<< liftIO (kitchenClean ps)
-    T.VtyEvent (V.EvKey (V.KChar 'e') _) -> nextServe ps   =<< liftIO (kitchenServe ps)
-    T.VtyEvent (V.EvKey (V.KChar ' ') _) -> nextRest ps   =<< liftIO (kitchenRest ps)
-    T.VtyEvent (V.EvKey (V.KChar 'v') _) -> nextConfirm ps   =<< liftIO (kitchenConfirm ps)
-    T.VtyEvent (V.EvKey V.KEnter _) -> nextRestart ps  =<< liftIO (kitchenRestart ps)
-
-    T.VtyEvent (V.EvKey V.KEsc _)   -> Brick.halt ps
-    _                               -> Brick.continue ps
+  T.VtyEvent (V.EvKey V.KUp _) -> Brick.continue (move up ps)
+  T.VtyEvent (V.EvKey V.KDown _) -> Brick.continue (move down ps)
+  T.VtyEvent (V.EvKey V.KLeft _) -> Brick.continue (move left ps)
+  T.VtyEvent (V.EvKey V.KRight _) -> Brick.continue (move right ps)
+  T.VtyEvent (V.EvKey (V.KChar 'a') _) -> nextPlace ps =<< liftIO (kitchenPlaceA ps)
+  T.VtyEvent (V.EvKey (V.KChar 'b') _) -> nextPlace ps =<< liftIO (kitchenPlaceB ps)
+  T.VtyEvent (V.EvKey (V.KChar 'p') _) -> nextPrepare ps =<< liftIO (kitchenPrepare ps)
+  T.VtyEvent (V.EvKey (V.KChar 'k') _) -> nextCook ps =<< liftIO (kitchenCook ps)
+  T.VtyEvent (V.EvKey (V.KChar 's') _) -> nextSeason ps =<< liftIO (kitchenSeason ps)
+  T.VtyEvent (V.EvKey (V.KChar 'c') _) -> nextClean ps =<< liftIO (kitchenClean ps)
+  T.VtyEvent (V.EvKey (V.KChar 'e') _) -> nextServe ps =<< liftIO (kitchenServe ps)
+  T.VtyEvent (V.EvKey (V.KChar ' ') _) -> nextRest ps =<< liftIO (kitchenRest ps)
+  T.VtyEvent (V.EvKey (V.KChar 'v') _) -> nextConfirm ps =<< liftIO (kitchenConfirm ps)
+  T.VtyEvent (V.EvKey V.KEnter _) -> nextRestart ps =<< liftIO (kitchenRestart ps)
+  T.VtyEvent (V.EvKey V.KEsc _) -> Brick.halt ps
+  _ -> Brick.continue ps
 
 move :: (Pos -> Pos) -> PlayerState -> PlayerState
-move f ps = ps { psPos = f (psPos ps) }
+move f ps = ps {psPos = f (psPos ps)}
 
 getStrat :: PlayerState -> Strategy
 getStrat ps = pStrat (psChef ps)
 
 getPos :: PlayerState -> IO Pos
-getPos ps  = getStrat ps (psPos ps) (psKitchen ps) 
+getPos ps = getStrat ps (psPos ps) (psKitchen ps)
 
-kitchenPlaceA :: PlayerState -> IO(Result Kitchen)
-kitchenPlaceA ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+-- * Handlers that catch & process player behaviors
+
+kitchenPlaceA :: PlayerState -> IO (Result Kitchen)
+kitchenPlaceA ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putIngredientA (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenPlaceB :: PlayerState -> IO(Result Kitchen)
-kitchenPlaceB ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+kitchenPlaceB :: PlayerState -> IO (Result Kitchen)
+kitchenPlaceB ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putIngredientB (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenPrepare :: PlayerState -> IO(Result Kitchen)
-kitchenPrepare ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+kitchenPrepare :: PlayerState -> IO (Result Kitchen)
+kitchenPrepare ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putPrepare (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenCook :: PlayerState -> IO(Result Kitchen)
-kitchenCook ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+kitchenCook :: PlayerState -> IO (Result Kitchen)
+kitchenCook ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putCook (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenSeason :: PlayerState -> IO(Result Kitchen)
-kitchenSeason ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+kitchenSeason :: PlayerState -> IO (Result Kitchen)
+kitchenSeason ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putSeason (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenClean :: PlayerState -> IO(Result Kitchen)
-kitchenClean ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+kitchenClean :: PlayerState -> IO (Result Kitchen)
+kitchenClean ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putClean (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenServe :: PlayerState -> IO(Result Kitchen)
-kitchenServe ps = 
-    if (pBudget (psChef ps)) == 0 then return Retry
+kitchenServe :: PlayerState -> IO (Result Kitchen)
+kitchenServe ps =
+  if (pBudget (psChef ps)) == 0
+    then return Retry
     else putServe (psKitchen ps) (psBudget ps) <$> getPos ps
 
-kitchenRestart ps =  putRestart (psKitchen ps) <$> getPos ps
+kitchenRestart ps = putRestart (psKitchen ps) <$> getPos ps
 
-kitchenConfirm ps =  putConfirm (psKitchen ps) <$> getPos ps
+kitchenConfirm ps = putConfirm (psKitchen ps) <$> getPos ps
 
 kitchenRest ps = putRest (psKitchen ps) <$> getPos ps
+
 -------------------------------------------------------------------------------
+
+-- * Handlers that update player state (budget, earning, etc.) after operations
+
 placeUpdatePS :: PlayerState -> PlayerState
 placeUpdatePS ps = ps {psBudget = budgetDown (psBudget ps) 20}
 
@@ -97,81 +106,93 @@ seasonUpdatePS :: PlayerState -> PlayerState
 seasonUpdatePS ps = ps {psBudget = budgetDown (psBudget ps) 5}
 
 serveUpdatePS :: PlayerState -> PlayerState
-serveUpdatePS ps = ps {psBudget = budgetDown (psBudget ps) 5,
-                        psEarning = add (psEarning ps)}
+serveUpdatePS ps =
+  ps
+    { psBudget = budgetDown (psBudget ps) 5,
+      psEarning = add (psEarning ps)
+    }
 
 cleanUpdatePS :: PlayerState -> PlayerState
 cleanUpdatePS ps = ps {psBudget = budgetDown (psBudget ps) 10}
 
 restUpdatePS :: PlayerState -> PlayerState
-restUpdatePS ps = ps {psBudget = budgetUp (psBudget ps) 100,
-              psDate = dateDown (psDate ps),
-              psBudgetLow = False}
+restUpdatePS ps =
+  ps
+    { psBudget = budgetUp (psBudget ps) 100,
+      psDate = dateDown (psDate ps),
+      psBudgetLow = False
+    }
 
 restartUpdatePS :: PlayerState -> PlayerState
-restartUpdatePS ps = ps {psBudget = budgetUp (psBudget ps) 100,
-                psDate = resetDate 3 (psDate ps),
-                psEarning = resetEarning (psEarning ps),
-                psOver = False }
+restartUpdatePS ps =
+  ps
+    { psBudget = budgetUp (psBudget ps) 100,
+      psDate = resetDate 3 (psDate ps),
+      psEarning = resetEarning (psEarning ps),
+      psOver = False
+    }
 
 needBudgetPS :: PlayerState -> Int -> PlayerState
-needBudgetPS ps b = if (getBudget (psBudget ps)) - b < 0 
-    then ps {psBudgetLow = True} 
+needBudgetPS ps b =
+  if (getBudget (psBudget ps)) - b < 0
+    then ps {psBudgetLow = True}
     else ps {psBudgetLow = False}
 
 confirmUpdatePS :: PlayerState -> PlayerState
 confirmUpdatePS ps = ps {psBudgetLow = False}
 
 overUpdatePS :: PlayerState -> PlayerState
-overUpdatePS ps = ps {psOver = True,
-            psEarning = setMaxEarning (psEarning ps)}            
+overUpdatePS ps =
+  ps
+    { psOver = True,
+      psEarning = setMaxEarning (psEarning ps)
+    }
 
 -------------------------------------------------------------------------------
+
+-- * Handlers for low budget cases
+
 nextPlace :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextPlace ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 20) else continue (placeUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 20) else continue (placeUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextPrepare :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextPrepare ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 5) else continue (prepareUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 5) else continue (prepareUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextCook :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextCook ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 10) else continue (cookUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 10) else continue (cookUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextSeason :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextSeason ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 5) else continue (seasonUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 5) else continue (seasonUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextServe :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextServe ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 5) else continue (serveUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 5) else continue (serveUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextClean :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextClean ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 10) else continue (cleanUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (result (psKitchen ps) == kitchen) then continue (needBudgetPS ps' 10) else continue (cleanUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextRest :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextRest ps kitchen = case nextMove ps kitchen of
-    Right ps' -> if ( getDate (psDate ps) == 0) then continue (overUpdatePS ps') else continue (restUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> if (getDate (psDate ps) == 0) then continue (overUpdatePS ps') else continue (restUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextConfirm :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextConfirm ps kitchen = case nextMove ps kitchen of
-    Right ps' -> continue (confirmUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
+  Right ps' -> continue (confirmUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
 
 nextRestart :: PlayerState -> Result Kitchen -> EventM n (Next PlayerState)
 nextRestart ps kitchen = case nextMove ps kitchen of
-    Right ps' -> continue (restartUpdatePS ps')
-    Left res -> halt (ps { psResult = res }) 
-
-
-
-
+  Right ps' -> continue (restartUpdatePS ps')
+  Left res -> halt (ps {psResult = res})
--- src/Model/Kitchen.hs
+++ src/Model/Kitchen.hs
@@ -1,40 +1,43 @@
 {-# LANGUAGE DeriveFunctor #-}
-module Model.Kitchen
-    ( -- * Types
-        Kitchen,
-        Dish (..),
-        Pos (..),
-        Result (..),
-      -- * Kitchen API
-        dim,
-        (!),
-        init,
-        putIngredientA,
-        putIngredientB,
-        putPrepare,
-        putCook,
-        putSeason,
-        putServe,
-        putRest,
-        putClean,
-        putConfirm,
-        positions,
-        putRestart,
-        emptyPositions,
-        takenPositions,
-        result,
-        -- * Pointer moves
-        up,
-        down,
-        left,
-        right
-    )
-    where
 
-import Prelude hiding (init)
+module Model.Kitchen
+  ( -- * Types
+    Kitchen,
+    Dish (..),
+    Pos (..),
+    Result (..),
+
+    -- * Kitchen API
+    dim,
+    (!),
+    init,
+    putIngredientA,
+    putIngredientB,
+    putPrepare,
+    putCook,
+    putSeason,
+    putServe,
+    putRest,
+    putClean,
+    putConfirm,
+    positions,
+    putRestart,
+    emptyPositions,
+    takenPositions,
+    result,
+
+    -- * Pointer moves
+    up,
+    down,
+    left,
+    right,
+  )
+where
+
+import qualified Data.Map as M
 import Model.Budget hiding (init)
 import Model.Earning hiding (init)
-import qualified Data.Map as M 
+import Prelude hiding (init)
 
 ------------------------------
 --  Kitchen Board
@@ -43,26 +46,27 @@ import qualified Data.Map as M
 type Kitchen = M.Map Pos Dish
 
 -- * Dishes
+
 data Dish
-    = IngredientA0        -- unprepared Ingredient for dish A (soup)
-    | IngredientA1        -- prepared Ingredient for dish A (soup)
-    | Prepared_raw_us_A -- prepared but still not-fully-cooked & unseasoned dish A
-    | Prepared_cooked_us_A  -- prepared & cooked but unseasoned dish A
-    | Prepared_raw_s_A -- prepared, seasoned but not-fully-cooked dish A
-    | DishA             -- Soup (good to serve)
-    | IngredientB         -- unprepared Ingredient for dish B (Sashimi)
-    | Prepared_us_B     -- prepared but unseasoned dish B
-    | DishB             -- Sashimi (ready to serve)
-    | Bad               -- Mistreated dishes
-    deriving (Eq, Show)
+  = IngredientA0 -- unprepared Ingredient for dish A (soup)
+  | IngredientA1 -- prepared Ingredient for dish A (soup)
+  | Prepared_raw_us_A -- prepared but still not-fully-cooked & unseasoned dish A
+  | Prepared_cooked_us_A -- prepared & cooked but unseasoned dish A
+  | Prepared_raw_s_A -- prepared, seasoned but not-fully-cooked dish A
+  | DishA -- Soup (good to serve)
+  | IngredientB -- unprepared Ingredient for dish B (Sashimi)
+  | Prepared_us_B -- prepared but unseasoned dish B
+  | DishB -- Sashimi (ready to serve)
+  | Bad -- Mistreated dishes
+  deriving (Eq, Show)
 
 -- * Positions
+
 data Pos = Pos
-    {
-        dishRow :: Int,       -- 1 <= dishRow <= dim
-        dishCol :: Int        -- 1 <= dishCol <= dim
-    }
-    deriving (Eq, Ord)
+  { dishRow :: Int, -- 1 <= dishRow <= dim
+    dishCol :: Int -- 1 <= dishCol <= dim
+  }
+  deriving (Eq, Ord)
 
 (!) :: Kitchen -> Pos -> Maybe Dish
 kitchen ! pos = M.lookup pos kitchen
@@ -86,110 +90,99 @@ init = M.empty
 --  Playing Logistics
 ------------------------------
 
-data Result a 
-    = Retry
-    | Cont a 
-    deriving (Eq, Functor, Show)
+data Result a
+  = Retry
+  | Cont a
+  deriving (Eq, Functor, Show)
+
+-- * Dish status update
 
 updateDishPrep :: Dish -> Maybe Dish
 updateDishPrep d =
-    case d of
-        IngredientA0      -> Just IngredientA1
-        IngredientB       -> Just Prepared_us_B
-        _               -> Just Bad
+  case d of
+    IngredientA0 -> Just IngredientA1
+    IngredientB -> Just Prepared_us_B
+    _ -> Just Bad
 
 updateDishCook :: Dish -> Maybe Dish
 updateDishCook d =
-    case d of
-        IngredientA1      -> Just Prepared_raw_us_A
-        Prepared_raw_us_A   -> Just Prepared_cooked_us_A
-        Prepared_raw_s_A    -> Just DishA
-        _                   -> Just Bad
+  case d of
+    IngredientA1 -> Just Prepared_raw_us_A
+    Prepared_raw_us_A -> Just Prepared_cooked_us_A
+    Prepared_raw_s_A -> Just DishA
+    _ -> Just Bad
 
 updateDishSeason :: Dish -> Maybe Dish
 updateDishSeason d =
-    case d of
-        Prepared_raw_us_A   -> Just Prepared_raw_s_A
-        Prepared_cooked_us_A -> Just DishA
-        Prepared_us_B       -> Just DishB
-        _                   -> Just Bad
+  case d of
+    Prepared_raw_us_A -> Just Prepared_raw_s_A
+    Prepared_cooked_us_A -> Just DishA
+    Prepared_us_B -> Just DishB
+    _ -> Just Bad
 
--- Operations
+-- Player operations
 putIngredientA :: Kitchen -> Budget -> Pos -> Result Kitchen
-putIngredientA kitchen buget pos = 
-    if (getBudget buget) >= 20
-        then
-            case M.lookup pos kitchen of
-                Nothing         -> result (M.insert pos IngredientA0 kitchen)
-                Just _          -> result kitchen
-        else
-            result kitchen
+putIngredientA kitchen buget pos =
+  if (getBudget buget) >= 20
+    then case M.lookup pos kitchen of
+      Nothing -> result (M.insert pos IngredientA0 kitchen)
+      Just _ -> result kitchen
+    else result kitchen
 
 putIngredientB :: Kitchen -> Budget -> Pos -> Result Kitchen
-putIngredientB kitchen buget pos = 
-    if (getBudget buget) >= 20
-        then
-            case M.lookup pos kitchen of
-                Nothing         -> result (M.insert pos IngredientB kitchen)
-                Just _          -> result kitchen
-        else
-            result kitchen
+putIngredientB kitchen buget pos =
+  if (getBudget buget) >= 20
+    then case M.lookup pos kitchen of
+      Nothing -> result (M.insert pos IngredientB kitchen)
+      Just _ -> result kitchen
+    else result kitchen
 
 putPrepare :: Kitchen -> Budget -> Pos -> Result Kitchen
-putPrepare kitchen buget pos = 
-    if (getBudget buget) >= 5
-        then
-            case M.lookup pos kitchen of
-                Just _          -> result (M.update updateDishPrep pos kitchen)
-                Nothing         -> result kitchen
-        else
-            result kitchen
+putPrepare kitchen buget pos =
+  if (getBudget buget) >= 5
+    then case M.lookup pos kitchen of
+      Just _ -> result (M.update updateDishPrep pos kitchen)
+      Nothing -> result kitchen
+    else result kitchen
 
 putSeason :: Kitchen -> Budget -> Pos -> Result Kitchen
-putSeason kitchen buget pos = 
-    if (getBudget buget) >= 5
-        then
-            case M.lookup pos kitchen of
-                Just _          -> result (M.update updateDishSeason pos kitchen)
-                Nothing         -> result kitchen
-        else
-            result kitchen
+putSeason kitchen buget pos =
+  if (getBudget buget) >= 5
+    then case M.lookup pos kitchen of
+      Just _ -> result (M.update updateDishSeason pos kitchen)
+      Nothing -> result kitchen
+    else result kitchen
 
 putCook :: Kitchen -> Budget -> Pos -> Result Kitchen
 putCook kitchen buget pos =
-    if (getBudget buget) >= 10
-        then
-            case M.lookup pos kitchen of
-                Just _          -> result (M.update updateDishCook pos kitchen)
-                Nothing         -> result kitchen
-        else
-            result kitchen
+  if (getBudget buget) >= 10
+    then case M.lookup pos kitchen of
+      Just _ -> result (M.update updateDishCook pos kitchen)
+      Nothing -> result kitchen
+    else result kitchen
 
 putServe :: Kitchen -> Budget -> Pos -> Result Kitchen
-putServe kitchen buget pos = 
-    if (getBudget buget) >= 5
-        then
-            case M.lookup pos kitchen of
-                Just DishA      -> result (M.delete pos kitchen)
-                Just DishB      -> result (M.delete pos kitchen)
-                _               -> result kitchen
-        else
-            result kitchen
+putServe kitchen buget pos =
+  if (getBudget buget) >= 5
+    then case M.lookup pos kitchen of
+      Just DishA -> result (M.delete pos kitchen)
+      Just DishB -> result (M.delete pos kitchen)
+      _ -> result kitchen
+    else result kitchen
 
 putClean :: Kitchen -> Budget -> Pos -> Result Kitchen
 putClean kitchen buget pos =
-    if (getBudget buget) >= 10
-        then
-            case M.lookup pos kitchen of
-                Just Bad        -> result (M.delete pos kitchen)
-                _               -> result kitchen
-        else
-            result kitchen
+  if (getBudget buget) >= 10
+    then case M.lookup pos kitchen of
+      Just Bad -> result (M.delete pos kitchen)
+      _ -> result kitchen
+    else result kitchen
 
--- * Rest 
+-- * Rest
+
 -- * Rest and clean remaining dishes in kitchen
 
-putRest :: Kitchen  -> Pos -> Result Kitchen
+putRest :: Kitchen -> Pos -> Result Kitchen
 putRest kitchen pos = result M.empty
 
 putConfirm :: Kitchen -> Pos -> Result Kitchen
@@ -200,31 +193,31 @@ putRestart _ _ = result M.empty
 
 result :: Kitchen -> Result Kitchen
 result b
-    | otherwise = Cont b 
+  | otherwise = Cont b
 
 ------------------------------
---  Player Moves
+--  Player Cursor Moves
 ------------------------------
 up :: Pos -> Pos
-up pos = pos 
-    {
-        dishRow = max 1 (dishRow pos - 1)
+up pos =
+  pos
+    { dishRow = max 1 (dishRow pos - 1)
     }
 
 down :: Pos -> Pos
-down pos = pos 
-    {
-        dishRow = min dim (dishRow pos + 1)
+down pos =
+  pos
+    { dishRow = min dim (dishRow pos + 1)
     }
 
 left :: Pos -> Pos
-left pos = pos 
-    {
-        dishCol = max 1 (dishCol pos - 1)
+left pos =
+  pos
+    { dishCol = max 1 (dishCol pos - 1)
     }
 
 right :: Pos -> Pos
-right pos = pos 
-    {
-        dishCol = min dim (dishCol pos + 1)
+right pos =
+  pos
+    { dishCol = min dim (dishCol pos + 1)
     }
